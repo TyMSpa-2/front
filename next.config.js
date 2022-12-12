33664fdb275e896707078ebe50d4a945ef234faa
Niko Mastakas <nicolasmastakas@gmail.com>
2022-12-12 07:38:30 -0300

    feat: create nextjs config for look pages in src

---
+++ next.config.js
@@ -0,0 +1,7 @@
+// next.config.js
+module.exports = {
+    //...
+    // Other Next.js configuration options
+    //...
+    pagePath: 'src/pages', // Tell Next.js where to find the "pages" folder
+};
