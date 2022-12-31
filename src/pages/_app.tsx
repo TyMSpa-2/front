c48d2516ec76ecad3beba5299a2083815782cab7
Alan Vilalte <alan9106@gmail.com>
2022-12-31 20:20:24 -0300

    feat: setup default styles and import them in _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { Provider } from 'react-redux'
 import store from '../app/store'
 import type { AppProps } from 'next/app'
+import '../public/styles.css';
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
